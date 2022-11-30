0d0ff3f8bf8aba107fe92b5639764d0c9d16f269
nadeemaghait <nadeem.ahmad@gcspvt.com>
2022-11-30 11:38:45 +0500

    django authentication

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -67,7 +67,7 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'category.context_processors.menu_links',
-                'carts.context_processors.counter', 
+                'carts.context_processors.counter',
             ],
         },
     },
@@ -132,3 +132,13 @@ STATICFILES_DIRS = [
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR /'media'
+
+from django.contrib.messages import constants as messages
+MESSAGE_TAGS = {
+    messages.ERROR: 'danger',
+}
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER  = 'shoppingarham@gmail.com'
+EMAIL_HOST_PASSWORD = '12345678@abcdef'
+EMAIL_USE_TLS = True
